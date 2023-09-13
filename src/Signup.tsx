ca8b861824fb64e0ebf991a3e45c78e243c494ac
PraffulPatel27 <prafful123patel@gmail.com>
2023-09-13 15:53:17 +0530

    Prafful starting commit first

--- src/Signup.tsx
+++ src/Signup.tsx
@@ -14,7 +14,7 @@ function Signup() {
       <form action="">
         <div>
           <div>
-            <h1>Name</h1>
+            <h1 className="bg-orange-300">Name</h1>
             <input
               type="text"
               className="block w-full py-2 text-gray-900 border-0 rounded-md shadow-sm outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
